1598d03aa2f28bc245914777207af3ae2f1c0fec
sbx_user1051 <sbx_user1051@169.254.9.45>
2019-04-14 14:34:49 +0000

    Sun Apr 14 14:34:49 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,19 +7,14 @@ describe Checkout do
   context "#illegal" do
     it "checks to valid items and fails" do
       expect(market.checkout("ABCDZ")).to eq(-1)
-    end
-
-    it "checks for valid items and passes" do
-      expect(market.checkout("ABC")).to eq(100)
-    end
+    end        
   end
 
   context "Correct basket sum" do
-
     it "Adds up a normal basket correctly" do
       expect(market.checkout("ABC")).to eq(100)
     end
-
   end
 
 end
+
