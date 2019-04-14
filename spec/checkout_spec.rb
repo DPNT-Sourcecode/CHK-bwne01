c2f3bc973b519095aafabba839237f878e8bbd49
sbx_user1051 <sbx_user1051@169.254.9.45>
2019-04-14 14:22:49 +0000

    Sun Apr 14 14:22:49 UTC 2019

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,18 +5,25 @@ describe Checkout do
   let (:market) { Checkout.new }
 
   context "#illegal" do
-
     it "checks to valid items and fails" do
       expect(market.checkout("ABCDZ")).to eq(-1)
     end
 
     it "checks for valid items and passes" do
-      expect(market.checkout("ADCCB")).to eq("Valid")
+      expect(market.checkout("ABC")).to eq(100)
+    end
+  end
+
+  context "Correct basket sum" do
+
+    it "Adds up a normal basket correctly" do
+      expect(market.checkout("ABC")).to eq(100)
     end
 
   end
 
 end
+
 
 
 
